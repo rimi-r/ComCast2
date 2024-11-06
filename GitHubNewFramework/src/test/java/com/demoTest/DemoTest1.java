7e1f57464162437f7d3969f8589f928c703ab6b1
rstar <rstar@Risp>
2024-11-06 13:14:12 +0530

    New repository

---
+++ GitHubNewFramework/src/test/java/com/demoTest/DemoTest1.java
@@ -0,0 +1,27 @@
+package com.demoTest;
+
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+
+		
+		@Test
+		public void demm()
+		{
+			System.out.println("helo");
+			System.out.println("fly");
+			System.out.println("test1");
+			System.out.println("test3");
+			System.out.println("test4");
+			System.out.println("test5");
+			System.out.println("test6");
+
+			System.out.println("clue6");
+			System.out.println("clue 7");
+			System.out.println("clue8");
+
+		}
+		
+	}
+
+
